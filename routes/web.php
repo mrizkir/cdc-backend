2a3c2c9a39fefd333731bcb8547fda4a7d51ecf6
Mochammad Rizki Romdoni <mr.romdoni@gmail.com>
2020-04-13 16:59:46 +0700

    route api

--- routes/web.php
+++ routes/web.php
@@ -24,43 +24,8 @@ $router->group(['prefix'=>'v1','middleware'=>'auth:api'], function () use ($rout
     $router->get('/auth/refresh',['uses'=>'AuthController@refresh','as'=>'auth.refresh']);
     $router->get('/auth/me',['uses'=>'AuthController@me','as'=>'auth.me']);
 
-
-    //data masters - opd
-    $router->post('/dmaster/opd',['middleware'=>['role:superadmin|bapelitbang|opd'],'uses'=>'DMaster\OrganisasiController@index','as'=>'opd.index']);    
-    $router->post('/dmaster/opd/loadopd',['middleware'=>['role:superadmin'],'uses'=>'DMaster\OrganisasiController@loadopd','as'=>'opd.loadopd']);    
-    $router->put('/dmaster/opd/{id}',['middleware'=>['role:superadmin|bapelitbang'],'uses'=>'DMaster\OrganisasiController@update','as'=>'opd.update']);
-    $router->get('/dmaster/opd/{id}/unitkerja',['middleware'=>['role:superadmin|bapelitbang|opd'],'uses'=>'DMaster\OrganisasiController@opdunitkerja','as'=>'opd.unitkerja']);
-    
-    //data masters - unit kerja
-    $router->post('/dmaster/unitkerja',['middleware'=>['role:superadmin|bapelitbang|opd'],'uses'=>'DMaster\SubOrganisasiController@index','as'=>'unitkerja.index']);    
-    $router->post('/dmaster/unitkerja/loadunitkerja',['middleware'=>['role:superadmin'],'uses'=>'DMaster\SubOrganisasiController@loadunitkerja','as'=>'unitkerja.loadunitkerja']);    
-    $router->put('/dmaster/unitkerja/{id}',['middleware'=>['role:superadmin|bapelitbang'],'uses'=>'DMaster\SubOrganisasiController@update','as'=>'unitkerja.update']);
-    
-    //data masters - jenis pelaksanaan
-    $router->post('/dmaster/jenispelaksanaan',['middleware'=>['role:superadmin|bapelitbang|opd|pptk'],'uses'=>'DMaster\JenisPelaksanaanController@index','as'=>'jenispelaksanaan.index']);    
-    $router->post('/dmaster/jenispelaksanaan/store',['middleware'=>['role:superadmin|bapelitbang'],'uses'=>'DMaster\JenisPelaksanaanController@store','as'=>'jenispelaksanaan.store']);
-    $router->put('/dmaster/jenispelaksanaan/{id}',['middleware'=>['role:superadmin|bapelitbang'],'uses'=>'DMaster\JenisPelaksanaanController@update','as'=>'jenispelaksanaan.update']);
-    $router->delete('/dmaster/jenispelaksanaan/{id}',['middleware'=>['role:superadmin|bapelitbang'],'uses'=>'DMaster\JenisPelaksanaanController@destroy','as'=>'jenispelaksanaan.destroy']);    
-
-    //belanja - rka murni
-    $router->post('/belanja/rkamurni',['middleware'=>['role:superadmin|bapelitbang|opd|pptk'],'uses'=>'Belanja\RKAMurniController@index','as'=>'rkamurni.index']);    
-    $router->get('/belanja/rkamurni/{id}',['middleware'=>['role:superadmin|bapelitbang|opd|pptk'],'uses'=>'Belanja\RKAMurniController@show','as'=>'rkamurni.show']);    
-    $router->put('/belanja/rkamurni/updateuraian/{id}',['middleware'=>['role:superadmin|bapelitbang|opd|pptk'],'uses'=>'Belanja\RKAMurniController@updateuraian','as'=>'rkamurni.updateuraian']);   
-    $router->post('/belanja/rkamurni/rencanatarget',['middleware'=>['role:superadmin|bapelitbang|opd|pptk'],'uses'=>'Belanja\RKAMurniController@rencanatarget','as'=>'rkamurni.rencanatarget']);        
-    $router->post('/belanja/rkamurni/savetargetfisik',['middleware'=>['role:superadmin|bapelitbang|opd|pptk'],'uses'=>'Belanja\RKAMurniController@savetargetfisik','as'=>'rkamurni.savetargetfisik']);        
-    $router->put('/belanja/rkamurni/updatetargetfisik',['middleware'=>['role:superadmin|bapelitbang|opd|pptk'],'uses'=>'Belanja\RKAMurniController@updatetargetfisik','as'=>'rkamurni.updatetargetfisik']);        
-    $router->get('/belanja/rkamurni/bulanrealisasi/{id}',['middleware'=>['role:superadmin|bapelitbang|opd|pptk'],'uses'=>'Belanja\RKAMurniController@bulanrealisasi','as'=>'rkamurni.bulanrealisasi']);        
-    $router->post('/belanja/rkamurni/savetargetanggarankas',['middleware'=>['role:superadmin|bapelitbang|opd|pptk'],'uses'=>'Belanja\RKAMurniController@savetargetanggarankas','as'=>'rkamurni.savetargetanggarankas']);        
-    $router->put('/belanja/rkamurni/updatetargetanggarankas',['middleware'=>['role:superadmin|bapelitbang|opd|pptk'],'uses'=>'Belanja\RKAMurniController@updatetargetanggarankas','as'=>'rkamurni.updatetargetanggarankas']);   
-    $router->post('/belanja/rkamurni/realisasi',['middleware'=>['role:superadmin|bapelitbang|opd|pptk'],'uses'=>'Belanja\RKAMurniController@realisasi','as'=>'rkamurni.realisasi']);             
-    $router->post('/belanja/rkamurni/saverealisasi',['middleware'=>['role:superadmin|bapelitbang|opd|pptk'],'uses'=>'Belanja\RKAMurniController@saverealisasi','as'=>'rkamurni.saverealisasi']);             
-    $router->put('/belanja/rkamurni/updaterealisasi/{id}',['middleware'=>['role:superadmin|bapelitbang|opd|pptk'],'uses'=>'Belanja\RKAMurniController@updaterealisasi','as'=>'rkamurni.updaterealisasi']);             
-    $router->delete('/belanja/rkamurni/deleterealisasi/{id}',['middleware'=>['role:superadmin|bapelitbang|opd|pptk'],'uses'=>'Belanja\RKAMurniController@destroy','as'=>'rkamurni.deleterealisasi']);             
-    $router->post('/belanja/rkamurni/loaddatakegiatanfirsttime',['middleware'=>['role:superadmin|bapelitbang|opd'],'uses'=>'Belanja\RKAMurniController@loaddatakegiatanFirsttime','as'=>'rkamurni.loaddatakegiatanfirsttime']);    
-    $router->post('/belanja/rkamurni/loaddatauraianfirsttime',['middleware'=>['role:superadmin|bapelitbang|opd'],'uses'=>'Belanja\RKAMurniController@loaddatauraianFirsttime','as'=>'rkamurni.loaddatauraianfirsttime']);    
-
     //setting - permissions
-    $router->get('/setting/permissions',['middleware'=>['role:superadmin|bapelitbang|opd'],'uses'=>'Setting\PermissionsController@index','as'=>'permissions.index']);
+    $router->get('/setting/permissions',['middleware'=>['role:superadmin|gugustugas|petugas'],'uses'=>'Setting\PermissionsController@index','as'=>'permissions.index']);
     $router->post('/setting/permissions/store',['middleware'=>['role:superadmin'],'uses'=>'Setting\PermissionsController@store','as'=>'permissions.store']);    
     $router->delete('/setting/permissions/{id}',['middleware'=>['role:superadmin'],'uses'=>'Setting\PermissionsController@destroy','as'=>'permissions.destroy']);
     
@@ -75,30 +40,25 @@ $router->group(['prefix'=>'v1','middleware'=>'auth:api'], function () use ($rout
     //setting - users
     $router->get('/setting/users',['middleware'=>['role:superadmin'],'uses'=>'Setting\UsersController@index','as'=>'users.index']);
     $router->post('/setting/users/store',['middleware'=>['role:superadmin'],'uses'=>'Setting\UsersController@store','as'=>'users.store']);
-    $router->post('/setting/users/storeuserpermissions',['middleware'=>['role:superadmin|bapelitbang|opd'],'uses'=>'Setting\UsersController@storeuserpermissions','as'=>'users.storeuserpermissions']);
-    $router->post('/setting/users/revokeuserpermissions',['middleware'=>['role:superadmin|bapelitbang|opd'],'uses'=>'Setting\UsersController@revokeuserpermissions','as'=>'users.revokeuserpermissions']);
+    $router->post('/setting/users/storeuserpermissions',['middleware'=>['role:superadmin|gugustugas|petugas'],'uses'=>'Setting\UsersController@storeuserpermissions','as'=>'users.storeuserpermissions']);
+    $router->post('/setting/users/revokeuserpermissions',['middleware'=>['role:superadmin|gugustugas|petugas'],'uses'=>'Setting\UsersController@revokeuserpermissions','as'=>'users.revokeuserpermissions']);
     $router->put('/setting/users/{id}',['middleware'=>['role:superadmin'],'uses'=>'Setting\UsersController@update','as'=>'users.update']);
     $router->delete('/setting/users/{id}',['middleware'=>['role:superadmin'],'uses'=>'Setting\UsersController@destroy','as'=>'users.destroy']);    
-    $router->get('/setting/users/{id}/permission',['middleware'=>['role:superadmin|bapelitbang|opd'],'uses'=>'Setting\UsersController@userpermissions','as'=>'users.permission']);    
-    $router->get('/setting/users/{id}/opd',['middleware'=>['role:superadmin|bapelitbang|opd|pptk'],'uses'=>'Setting\UsersController@useropd','as'=>'users.opd']);    
+    $router->get('/setting/users/{id}/permission',['middleware'=>['role:superadmin|gugustugas|petugas'],'uses'=>'Setting\UsersController@userpermissions','as'=>'users.permission']);    
+    $router->get('/setting/users/{id}/petugas',['middleware'=>['role:superadmin|gugustugas|petugas|pptk'],'uses'=>'Setting\UsersController@userpetugas','as'=>'users.petugas']);    
     
-    //setting - users bapelitbang
-    $router->get('/setting/usersbapelitbang',['middleware'=>['role:superadmin|bapelitbang'],'uses'=>'Setting\UsersBapelitbangController@index','as'=>'usersbapelitbang.index']);
-    $router->post('/setting/usersbapelitbang/store',['middleware'=>['role:superadmin|bapelitbang'],'uses'=>'Setting\UsersBapelitbangController@store','as'=>'usersbapelitbang.store']);
-    $router->put('/setting/usersbapelitbang/{id}',['middleware'=>['role:superadmin|bapelitbang'],'uses'=>'Setting\UsersBapelitbangController@update','as'=>'usersbapelitbang.update']);
-    $router->put('/setting/usersbapelitbang/{id}',['middleware'=>['role:superadmin|bapelitbang'],'uses'=>'Setting\UsersBapelitbangController@update','as'=>'usersbapelitbang.update']);
-    $router->delete('/setting/usersbapelitbang/{id}',['middleware'=>['role:superadmin|bapelitbang'],'uses'=>'Setting\UsersBapelitbangController@destroy','as'=>'usersbapelitbang.destroy']);    
+    //setting - users gugustugas
+    $router->get('/setting/usersgugustugas',['middleware'=>['role:superadmin|gugustugas'],'uses'=>'Setting\UsersGugusTugasController@index','as'=>'usersgugustugas.index']);
+    $router->post('/setting/usersgugustugas/store',['middleware'=>['role:superadmin|gugustugas'],'uses'=>'Setting\UsersGugusTugasController@store','as'=>'usersgugustugas.store']);
+    $router->put('/setting/usersgugustugas/{id}',['middleware'=>['role:superadmin|gugustugas'],'uses'=>'Setting\UsersGugusTugasController@update','as'=>'usersgugustugas.update']);
+    $router->put('/setting/usersgugustugas/{id}',['middleware'=>['role:superadmin|gugustugas'],'uses'=>'Setting\UsersGugusTugasController@update','as'=>'usersgugustugas.update']);
+    $router->delete('/setting/usersgugustugas/{id}',['middleware'=>['role:superadmin|gugustugas'],'uses'=>'Setting\UsersGugusTugasController@destroy','as'=>'usersgugustugas.destroy']);    
     
-    //setting - users opd
-    $router->get('/setting/usersopd',['middleware'=>['role:superadmin|bapelitbang|opd'],'uses'=>'Setting\UsersOPDController@index','as'=>'usersopd.index']);
-    $router->post('/setting/usersopd/store',['middleware'=>['role:superadmin|bapelitbang|opd'],'uses'=>'Setting\UsersOPDController@store','as'=>'usersopd.store']);
-    $router->put('/setting/usersopd/{id}',['middleware'=>['role:superadmin|bapelitbang|opd'],'uses'=>'Setting\UsersOPDController@update','as'=>'usersopd.update']);
-    $router->put('/setting/usersopd/{id}',['middleware'=>['role:superadmin|bapelitbang|opd'],'uses'=>'Setting\UsersOPDController@update','as'=>'usersopd.update']);
-    $router->delete('/setting/usersopd/{id}',['middleware'=>['role:superadmin|bapelitbang|opd'],'uses'=>'Setting\UsersOPDController@destroy','as'=>'usersopd.destroy']);    
-    
-    //setting - menus
-    $router->get('/setting/menus',['middleware'=>['role:superadmin'],'uses'=>'Setting\MenusController@index','as'=>'menus.index']);
-    $router->post('/setting/menus/store',['middleware'=>['role:superadmin'],'uses'=>'Setting\MenusController@store','as'=>'menus.store']);
-    $router->put('/setting/menus/{id}',['middleware'=>['role:superadmin'],'uses'=>'Setting\MenusController@update','as'=>'menus.update']);
-    $router->delete('/setting/menus/{id}',['middleware'=>['role:superadmin'],'uses'=>'Setting\MenusController@destroy','as'=>'menus.destroy']);    
+    //setting - users petugas
+    $router->get('/setting/userspetugas',['middleware'=>['role:superadmin|gugustugas|petugas'],'uses'=>'Setting\UsersPetugasController@index','as'=>'userspetugas.index']);
+    $router->post('/setting/userspetugas/store',['middleware'=>['role:superadmin|gugustugas|petugas'],'uses'=>'Setting\UsersPetugasController@store','as'=>'userspetugas.store']);
+    $router->put('/setting/userspetugas/{id}',['middleware'=>['role:superadmin|gugustugas|petugas'],'uses'=>'Setting\UsersPetugasController@update','as'=>'userspetugas.update']);
+    $router->put('/setting/userspetugas/{id}',['middleware'=>['role:superadmin|gugustugas|petugas'],'uses'=>'Setting\UsersPetugasController@update','as'=>'userspetugas.update']);
+    $router->delete('/setting/userspetugas/{id}',['middleware'=>['role:superadmin|gugustugas|petugas'],'uses'=>'Setting\UsersPetugasController@destroy','as'=>'userspetugas.destroy']);    
+  
 });
