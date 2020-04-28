b43ec37431b1a7a865f225c497391b65809b6ec5
Mochammad Rizki Romdoni <mr.romdoni@gmail.com>
2020-04-28 22:31:51 +0700

    typo 1

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\User;
-use App\Models\Dmaster\StatusPasienModel;
+use App\Models\DMaster\StatusPasienModel;
 
 
 class AuthController extends Controller
